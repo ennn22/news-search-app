37a8e1d093223fae06e926fca440239979de516a
en <huien122@gmail.com>
2023-10-17 00:14:41 +0800

    Styled for login page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './Components/Login';
+import Home from './Components/Home';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" />
-        <Route path="/home" />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   )
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,5 +1,63 @@
+import { useState, useNavigate } from 'react';
+import { Box, Container, Grid, TextField, Typography } from '@mui/material/';
 
-const Login = () => {}
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    
+    // if (isLoggedIn) 
+    //   return navigate('/home');
+  };
+
+  return (
+    <div className='loginBigBox'>
+      <Box className='loginHeader'>
+        <Typography variant="h2">Daily News</Typography>
+        <Typography variant="h4" sx={{ fontWeight: '600' }}>Login</Typography>
+      </Box>
+      <form className="loginBox" onSubmit={handleLogin}>
+        <Grid 
+          container 
+          spacing={2} 
+          direction="column" 
+          justifyContent="center"
+          alignItems="center">
+          <Grid item xs={6}>
+            <TextField
+              id="standard-basic"
+              label="Username"
+              type="text"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              margin="dense"
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              id="standard-basic"
+              label="Password"
+              type="password"
+              name="password"
+
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              margin="dense"
+            />
+          </Grid>
+        </Grid>
+      </form>
+      <div className='loginButton'>
+        <button type="submit">Login</button>
+      </div>
+    </div>
+  )
+}
+
 
 
 export default Login;
